be3cba97b8cedf9f2c9a5c4f97f370d5e386ce83
NIYOBYOSE-Isaac-Precieux <isaprecieux112@gmail.com>
2026-01-17 19:49:00 +0200

    Apis being tested

--- src/main/java/com/ecommerce/EcomAppApplication.java
+++ src/main/java/com/ecommerce/EcomAppApplication.java
@@ -2,12 +2,16 @@ package com.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EntityScan("com.ecommerce.entity")
+@EnableJpaRepositories("com.ecommerce.repository")
 public class EcomAppApplication {
 
 	public static void main(String[] args) {
