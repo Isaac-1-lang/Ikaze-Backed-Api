cc2607a5c4279731e3527a86ae36738a4807d67c
NIYOBYOSE-Isaac-Precieux <isaprecieux112@gmail.com>
2026-04-22 14:28:08 +0200

    Initial commit

---
+++ src/main/java/com/ikaze/IkazeApplication.java
@@ -0,0 +1,14 @@
+package com.ikaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkazeApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        SpringApplication.run(IkazeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ikaze/IkazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkazeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
